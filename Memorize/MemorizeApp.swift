ad39687be4b1610ea78109d6901888e59a513d2e
Aysun Gorkem <35496294+AysunGorkem@users.noreply.github.com>
2024-11-16 08:38:56 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aysun Görkem on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by Aysun Görkem on 16/11/2024.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
