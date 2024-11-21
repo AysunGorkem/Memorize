67412a912704c8e71ff4f52f15c8212d1dc8583e
Aysun Gorkem <35496294+AysunGorkem@users.noreply.github.com>
2024-11-21 09:50:48 +0300

    Togglable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,8 +12,8 @@ struct ContentView: View {
         HStack{
             CardView(isFaceUp: true)
             CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
         }
 
         .foregroundColor(.orange)
@@ -22,21 +22,21 @@ struct ContentView: View {
 }
 
 struct CardView : View {
-    var isFaceUp : Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
-        ZStack (content: {
+        ZStack (alignment: .center){
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("Ays❤️").font(.largeTitle)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("❤️").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
-            
-        })
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 #Preview {
